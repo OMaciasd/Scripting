001a87d48a8dbdbc9dc88a4711cadab8ddca4f9f
Oscar Macias <79873527+OMaciasd@users.noreply.github.com>
2023-08-25 11:24:51 -0500

    🚑 cd(main) Change lfa.sh - $ENV-functions - 2
    🚑 cd(main) Change lfa.sh - $ENV-functions

--- Azure/lfa.sh
+++ Azure/lfa.sh
@@ -2,12 +2,13 @@
 
 # ENV.
 funtionappName=
-RESOURCEGROUP=rginfradevops
+RESOURCEGROUP=
+LOCATION=
 
 # SERVICE PLAN.
-SERVICEPLAN_DEV=app-dev
-SERVICEPLAN_STA=app-sta
-SERVICEPLAN_PRD=app-prd
+SERVICEPLAN_DEV=
+SERVICEPLAN_STA=
+SERVICEPLAN_PRD=
 
 # STORAGE ACCOUNT.
 STORAGE_DEV=dev${funtionappName}functions
@@ -16,17 +17,17 @@ STORAGE_PROD=prd${funtionappName}functions
 
 # AZURE RESOURCES.
 az account set --subscription "$AZURE_SUSCRIPTION"
-az group create --name "$RESOURCEGROUP" --location 'eastus'
+az group create --name "$RESOURCEGROUP" --location '$LOCATION'
 
 # AZURE RESOURCES: SERVICE PLAN.
-az appservice plan create --name "$SERVICEPLAN_DEV" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location 'eastus'
-az appservice plan create --name "$SERVICEPLAN_STA" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location 'eastus'
-az appservice plan create --name "$SERVICEPLAN_PRD" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location 'eastus'
+az appservice plan create --name "$SERVICEPLAN_DEV" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location '$LOCATION'
+az appservice plan create --name "$SERVICEPLAN_STA" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location '$LOCATION'
+az appservice plan create --name "$SERVICEPLAN_PRD" --resource-group "$RESOURCEGROUP" --sku B3 --is-linux --location '$LOCATION'
 
 # AZURE RESOURCES: STORAGE ACCOUNT.
-az storage account create --name "$STORAGE_DEV" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=dev
-az storage account create --name "$STORAGE_STA" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=sta
-az storage account create --name "$STORAGE_PROD" --location 'eastus' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=prod
+az storage account create --name "$STORAGE_DEV" --location '$LOCATION' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=dev
+az storage account create --name "$STORAGE_STA" --location '$LOCATION' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=sta
+az storage account create --name "$STORAGE_PROD" --location '$LOCATION' --resource-group "$RESOURCEGROUP" --sku Standard_LRS --allow-blob-public-access false --tags created_by=azure environment=prod
 
 # DEV ENVIRONMENT.
 az functionapp create --resource-group "$RESOURCEGROUP" \
